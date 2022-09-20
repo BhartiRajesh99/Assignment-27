1e137d6c8a3fd6e65a5e8abe47c14bc266871c62
RAJ <109366789+BhartiRajesh99@users.noreply.github.com>
2022-09-20 21:42:19 +0530

    Update 2-IncrementDecrement.cpp

--- 2-IncrementDecrement.cpp
+++ 2-IncrementDecrement.cpp
@@ -29,10 +29,24 @@ class Complex
             T.img=img;
             return T;
         }
+        Complex operator++(int dummy)
+        {
+            Complex T;
+            T.real=real++;
+            T.img=img;
+            return T;
+        }
         Complex operator--()
         {
             Complex Z;
             Z.real=--real;
+            Z.img=img;
+            return Z;
+        }
+        Complex operator--(int dummy)
+        {
+            Complex Z;
+            Z.real=real--;
             Z.img=img;
             return Z;
         }
@@ -43,7 +57,7 @@ int main()
     c1.setData(4,5);
     c2=++c1;    //c2=c1.operator++();
     c2.showData();
-    c2=--c1;    //c2=c1.operator--();
-    c2.showData();
+    c2=c1--;    //c2=c1.operator--();
+    c1.showData();
     return 0;
 }
